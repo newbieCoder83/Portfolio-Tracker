90da6b069d89e5180196bd7dedfd91c851bdc0ab
newbieCoder83 <afraz12123@gmail.com>
2026-04-22 00:25:20 +0100

    refactor: memoize HighchartsGrid themed options

--- client/src/components/HighchartsGrid.jsx
+++ client/src/components/HighchartsGrid.jsx
@@ -1,9 +1,11 @@
+import { useMemo } from 'react';
 import { Grid } from '@highcharts/grid-lite-react';
 
 export default function HighchartsGrid({ options }) {
-  const themed = {
+  const themed = useMemo(() => ({
     ...options,
     rendering: { ...options?.rendering, theme: 'hcg-theme-default hcg-dark' },
-  };
+  }), [options]);
+
   return <Grid options={themed} />;
 }
